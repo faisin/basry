ab381f5c4c688e19a77def4113893595040bc43f
faisin <83275517+faisin@users.noreply.github.com>
2026-09-25 00:08:52 +0800

    Create repo-config.sh

---
+++ lib/repo-config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# AutoScriptXray project repository configuration.
+# Keep all first-party downloads pinned to this repository and branch.
+export REPO_OWNER="${REPO_OWNER:-faisin/basry}"
+export REPO_BRANCH="${REPO_BRANCH:-main}"
+export REPO_RAW="https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_BRANCH}"
+export REPO_WEB="https://github.com/${REPO_OWNER}"
+
